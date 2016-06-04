58a5fa24f8a0d2c155efe7860b84b5b43b2d51c0
christophelec <christophe.lecointe@tuta.io>
2016-06-04 16:43:14 +0200

    Base compose for the application is working

---
+++ compose/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def ping():
+	redis.incr('hits')
+	return '%s ping received. About as much pong returned.' % redis.get('hits')
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
